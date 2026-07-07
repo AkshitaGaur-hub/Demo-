bd3c2d78aa989d79e36ec705933472b54275dfd9
AkshitaGaur-hub <gaurakshita407@gmail.com>
2026-07-07 11:32:25 +0530

    Setup Project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div>
+      <h1 className='font-bold text-2xl bg-red-600 text-white'>Hello Mamta And Garmia</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
+
