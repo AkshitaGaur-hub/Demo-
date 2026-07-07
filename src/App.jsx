632c8d86b23c68f4f0f344295fe215091cc5dc74
Mamta Bhati <mamtabhatics44@gmail.com>
2026-07-07 13:29:12 +0530

    Rename the searchBox

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import SearchBox from "./SearchBox";
 
 const App = () => {
   return (
@@ -7,7 +8,8 @@ const App = () => {
         <h1 className='p-10 font-bold text-2xl text-black text-center'>
           Hello Mamta And Garmia
         </h1>
-
+        
+       <SearchBox />
         <button 
           type="button" 
           className="text-white rounded-2xl bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium text-sm px-4 py-2.5 text-center leading-5"
--- src/searchBox.jsx
+++ src/searchBox.jsx
@@ -1,5 +1,5 @@
 import TextField from '@mui/material/TextField';
-import './searchBox.css';
+import './SearchBox.css';
 
 const SearchBox = () => {
   return (
